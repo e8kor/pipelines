43572bb4ca111d1bc257a26091000e90ee59c2a1
IEvgenii Korniichuk <nutscracker.ua@gmail.com>
2021-01-10 14:35:34 +0100

    removed actions

--- images/airflow/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-[[ -z "$AIRFLOW__CORE__FERNET_KEY" ]] && { echo "Fernet key for AIRFLOW__CORE__FERNET_KEY is not defined"; exit 1; }
-
-airflow initdb
-airflow scheduler &
-exec airflow webserver
