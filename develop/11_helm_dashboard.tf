4548ffeb6c481a321641eea7a0fc958f147b27b6
IEvgenii Korniichuk <nutscracker.ua@gmail.com>
2021-01-16 21:33:28 +0100

    cleanup

--- develop/11_helm_dashboard.tf
+++
@@ -1,8 +0,0 @@
-resource "helm_release" "dashboard" {
-  name      = "dashboard"
-  namespace = "kubernetes-dashboard"
-
-  repository = "https://kubernetes.github.io/dashboard"
-  chart      = "kubernetes-dashboard"
-
-}
