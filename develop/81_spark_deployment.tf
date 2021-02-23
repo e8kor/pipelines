bf3abe219f4422b192263e1191d8708f249dbdca
IEvgenii Korniichuk <nutscracker.ua@gmail.com>
2021-02-23 21:41:44 +0100

    Commit Message Title
    Why:
    
    * fix deployment files
    
    This change addresses the need by:
    
    * Remove bucket creation, job image is incompatible with arm
    * Define kubernetes cluster properties

--- develop/81_spark_deployment.tf
+++ develop/81_spark_deployment.tf
@@ -19,23 +19,39 @@ resource "kubernetes_config_map" "master-spark-defaults" {
   }
   data = {
     "spark-defaults.conf" = <<EOT
-        spark.jars.packages org.apache.hadoop:hadoop-aws:3.2.0,org.apache.hadoop:hadoop-common:3.2.0,com.amazonaws:aws-java-sdk-bundle:1.11.874
-        spark.kubernetes.authenticate.driver.serviceAccountName spark
-        spark.kubernetes.namespace spark
-        spark.master spark://0.0.0.0:7077
-        spark.driver.extraLibraryPath /opt/hadoop/lib/native
-        spark.app.id KubernetesSpark
-        spark.hadoop.fs.s3a.endpoint http://external-storage.storage:9000
-        spark.hadoop.fs.s3a.access.key ${var.storage-access-key}
-        spark.hadoop.fs.s3a.secret.key ${random_password.storage.result}
-        spark.hadoop.fs.s3a.path.style.access true
-        spark.hadoop.fs.s3a.impl org.apache.hadoop.fs.s3a.S3AFileSystem
+spark.jars.packages org.apache.hadoop:hadoop-aws:3.2.0,org.apache.hadoop:hadoop-common:3.2.0,com.amazonaws:aws-java-sdk-bundle:1.11.874
+spark.app.id KubernetesSpark
+spark.master k8s://https://kubernetes.default
+spark.kubernetes.pyspark.pythonVersion 3
+spark.kubernetes.executor.label.spark executor
+spark.kubernetes.container.image e8kor/spark-py:3.0.2
+spark.kubernetes.authenticate.driver.serviceAccountName spark
+spark.kubernetes.namespace spark
+spark.submit.deployMode client
+spark.driver.port 7778
+spark.driver.extraLibraryPath /opt/hadoop/lib/native
+spark.hadoop.fs.s3a.endpoint http://external-storage.storage:9000
+spark.hadoop.fs.s3a.access.key ${var.storage-access-key}
+spark.hadoop.fs.s3a.secret.key ${random_password.storage.result}
+spark.hadoop.fs.s3a.path.style.access true
+spark.hadoop.fs.s3a.impl org.apache.hadoop.fs.s3a.S3AFileSystem
       EOT
   }
 }
 
-# fs.s3a.access.key minio
-# fs.s3a.secret.key e7oJO2QrLCHhd9jW
+# "spark.master”,”k8s://https://kubernetes.default" 
+# "spark.app.name”, <your_app_name>” 
+# "spark.kubernetes.executor.label.<label_name>","<label_value>
+# "spark.driver.host”, <the pod's ip address>
+# "spark.kubernetes.namespace”, <namespace>
+# "spark.submit.deployMode”, “client” 
+# "spark.kubernetes.container.image", "<executor image name from above>" 
+# "spark.kubernetes.pyspark.pythonVersion”, “3” 
+# "spark.driver.port”, “7778” 
+# "spark.kubernetes.container.image.pullSecrets", "<secret name>"
+# "spark.jars”,”/usr/local/spark/jars/hadoop-aws-2.9.2.jar,/usr/local/spark/jars/aws-java-sdk-1.7.4.jar
+# fs.s3a.access.key ****
+# fs.s3a.secret.key ****
 # fs.s3a.endpoint http://external-storage.storage:9000
 # file("${path.module}/spark/master-spark-defaults.conf")
 
@@ -101,7 +117,7 @@ module "spark-worker" {
   image_version = "3.0.2"
   internal_tcp  = [8081]
   command       = ["/opt/spark/bin/spark-class", "org.apache.spark.deploy.worker.Worker", "spark://spark-master-tcp-0:7077", "--webui-port", "8081"]
-  replicas      = 2
+  replicas      = 0
   cpu           = "100m"
   node_selector = {
     "node-role.kubernetes.io/spark" = ""
